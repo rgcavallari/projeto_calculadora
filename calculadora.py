4c561f7e11298de297b7826ba54bba9360eecbcf
Ana Carolina Antunes Naime <ana.naime@anchieta.br>
2025-11-29 11:25:22 -0300

    Atlético mineiro é o melhor

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,12 @@
-a = float(input('Entre com o primeiro número: '))
-b = float(input('Entre com o segundo número: '))
+a = float(input('Entre com o seu primeiro número: '))
+b = float(input('Entre com o seu segundo número: '))
 soma = a + b
 subtracao = a - b
 multiplicacao = a*b
 divisao = a/b
+potência = a**b
 print(f'A soma é {soma}.')
 print(f'A diferença é {subtracao}.')
 print(f'A multiplicação é {multiplicacao}.')
 print(f'A divisão é {divisao}.')
+print(f"A potência é {potência}.')

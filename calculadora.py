affd0f111503ceaefa7190138898d209e36b080d
CARLOS HENRIQUE NERI DE OLIVEIRA (2516892) <2516892@edu.anchieta.br>
2025-11-29 11:26:08 -0300

    Calculadora de playboy

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,7 @@
 a = float(input('Entre com o primeiro número: '))
 b = float(input('Entre com o segundo número: '))
 soma = a + b
-subtracao = a - b
 multiplicacao = a*b
-divisao = a/b
 print(f'A soma é {soma}.')
 print(f'A diferença é {subtracao}.')
 print(f'A multiplicação é {multiplicacao}.')

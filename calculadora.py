168c80ae554f93a0092049f87cfd3e7c378a664c
luxzfi <luquetesan@gmail.com>
2025-11-29 11:36:27 -0300

    santos 3x0

--- calculadora.py
+++ calculadora.py
@@ -5,6 +5,5 @@ subtracao = a - b
 multiplicacao = a*b
 divisao = a/b
 print(f'A soma é {soma}.')
-print(f'A diferença é {subtracao}.')
 print(f'A multiplicação é {multiplicacao}.')
 print(f'A divisão é {divisao}.')

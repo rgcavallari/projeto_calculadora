80955987453e1f2714270a6d479fef420cfedf77
GUSTAVO SANTOS OLIVEIRA (2508451) <2508451@edu.anchieta.br>
2025-11-29 11:27:55 -0300

    Santos 3 x o com show de Neymar

--- calculadora.py
+++ calculadora.py
@@ -2,8 +2,8 @@ a = float(input('Entre com o primeiro número: '))
 b = float(input('Entre com o segundo número: '))
 soma = a + b
 subtracao = a - b
-multiplicacao = a*b
 divisao = a/b
+multiplicacao = a*b
 print(f'A soma é {soma}.')
 print(f'A diferença é {subtracao}.')
 print(f'A multiplicação é {multiplicacao}.')

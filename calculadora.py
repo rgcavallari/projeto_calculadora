35f8805e9181ca5aaaf55a99cea91859506ea2d7
Rafael <rafaelcavallari@gmail.com>
2025-11-29 10:18:04 -0300

    Minha segunda versão da calculadora

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,9 @@ a = float(input('Entre com o primeiro número: '))
 b = float(input('Entre com o segundo número: '))
 soma = a + b
 subtracao = a - b
+multiplicacao = a*b
+divisao = a/b
 print(f'A soma é {soma}.')
-print(f'A diferença é {subtracao}')
+print(f'A diferença é {subtracao}.')
+print(f'A multiplicação é {multiplicacao}.')
+print(f'A divisão é {divisao}.')

68f2035db8523bc0dc5978926d6a6497ad9cd0b3
Rafael <rafaelcavallari@gmail.com>
2025-11-29 10:14:13 -0300

    Minha primeira versão da calculadora

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+a = float(input('Entre com o primeiro número: '))
+b = float(input('Entre com o segundo número: '))
+soma = a + b
+subtracao = a - b
+print(f'A soma é {soma}.')
+print(f'A diferença é {subtracao}')
